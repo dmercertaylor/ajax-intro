1ae9761b64c7c8f212c92858511332eb6ed48d2f
dmercertaylor <dmercertaylor01@hamline.edu>
2019-11-13 16:14:11 -0600

    added sample ajax

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,15 @@
-console.log('js');
+$(document).ready( onReady );
+
+function onReady(){
+    console.log('js');
+
+    $.ajax({
+        method: 'GET',
+        url: '/cohorts'
+    }).then(function(response){
+        console.log(response);
+    }).catch(function(error){
+        console.log(`Error on request to server`, error);
+        alert('Something went wrong');
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-
 const express = require('express');
 const app = express();
 
 const port = 5000;
 
-app.use(express.static('server/public'));
+app.use( express.static('server/public') );
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}...`);
+});
+
+// Some data...
+const cohorts = ['Trifid', 'Playfair', 'Atbash', 'Sirius', 'Menkar', 'Gacrux'];
+
+// GET request - sends back all cohorts
+app.get('/cohorts', (req, res)=>{
+    res.send(cohorts);
 });
